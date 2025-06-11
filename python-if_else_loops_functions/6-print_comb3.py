2970e8911f81aeb1e62e3d07e6ca08fab798628e
VidadiJavadov <vidadi4000@gmail.com>
2025-06-11 18:32:31 +0400

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,8 @@ for i in range(10):
     for j in range(10):
         if i < j:
             if i == 8 and j == 9:
-                print("{0}{1}".format(i,j))
+                print("{:02}{:02}".format(i, j))
             else:
-                print("{}{}".format(i,j), end=", ")
+                print("{:02}{:02}".format(i, j), end=", ")
+
 
