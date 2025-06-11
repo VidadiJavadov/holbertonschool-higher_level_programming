4aa53829677d431e552d1e8d3ef6577f3fa3029f
VidadiJavadov <vidadi4000@gmail.com>
2025-06-11 18:30:53 +0400

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,5 @@ for i in range(10):
             if i == 8 and j == 9:
                 print("{0}{1}".format(i,j))
             else:
-                print("{}{}".format(i,j),end=", ")
+                print("{}{}".format(i,j), end=", ")
+
