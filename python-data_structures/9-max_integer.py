5a6124b790ee1aa6dc93784ec81985fa1f7e94ad
VidadiJavadov <vidadi4000@gmail.com>
2025-06-18 19:16:46 +0400

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for num in my_list:
-        if num % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return new_list
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = my_list[0]
+    for el in my_list:
+        if(max < el):
+            max = el
+    return max
+
+print(max_integer([]))
