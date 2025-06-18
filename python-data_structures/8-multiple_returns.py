b27c412487559ab7ac04ef0e833ce2f7cc1f8439
VidadiJavadov <vidadi4000@gmail.com>
2025-06-18 18:56:01 +0400

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if(sentence == ""):
-        r_tuple = (len(sentence), None)
-    else:
-        r_tuple = (len(sentence), sentence[0])
-    return r_tuple
+    if not sentence:
+        return (0, None)
+    return (len(sentence), sentence[0])
