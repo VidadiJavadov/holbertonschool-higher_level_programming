1c00bdc4c8f4b30dbe2a7f30b1ffdba53062226d
VidadiJavadov <vidadi4000@gmail.com>
2025-06-18 16:28:37 +0400

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def new_in_list(my_list, idx, element):
-    new_list = list(my_list)
-    if(idx < 0 or idx >= len(my_list)):
-        return new_list
-    else:
-        new_list[idx] = element
-        return new_list
+    """Replaces an element in a copy of a list at a specific position."""
+    list_copy = my_list.copy()
+    if idx < 0 or idx >= len(list_copy):
+        return list_copy
+    list_copy[idx] = element
+    return list_copy
