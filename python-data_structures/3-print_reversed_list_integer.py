ca9c130517ec56a0f1e6f5ee3cebefdef3b030af
Vidadi1918 <157149170+Vidadi1918@users.noreply.github.com>
2025-06-18 16:04:34 +0400

    Reverse

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/bash
 
 def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list)-1, -1, -1):
-        print(my_list[i])
+        print("{:d}".format(my_list[i]))
 
 print_reversed_list_integer([1,2,3,4])
