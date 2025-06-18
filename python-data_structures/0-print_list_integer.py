91c9a7a0c001992936870f1cfbf970512adef69c
VidadiJavadov <vidadi4000@gmail.com>
2025-06-18 06:11:25 +0400

    0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@ def print_list_integer(my_list=[]):
     for items in my_list:
         print(itemsdef print_list_integer(my_list=[]):
     for items in my_list:
-        print("{}".format(items))
+        print("{:d}".format(items))
