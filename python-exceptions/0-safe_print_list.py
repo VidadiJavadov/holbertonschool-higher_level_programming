c6394d51887d2bcb3cc7feaa0a25514f20e444a1
VidadiJavadov <vidadi4000@gmail.com>
2025-06-20 17:17:18 +0400

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            i += 1
-        except:
-            break
-    print()
-    return i
+    count = 0
+    try:
+        for i in range(x):
+            try:
+                print(my_list[i], end="")
+                count += 1
+            except IndexError:
+                break
+    finally:
+        print()
+        return count
