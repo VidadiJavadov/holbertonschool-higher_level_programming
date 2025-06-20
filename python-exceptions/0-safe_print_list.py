78003c6c1d25e5b8287402e3804de92b3dd6b85b
VidadiJavadov <vidadi4000@gmail.com>
2025-06-20 17:15:06 +0400

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        new_list = my_list[:x]
-        return new_list
-    except:
-        print("stack overflow")
+    i = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            i += 1
+        except:
+            break
+    print()
+    return i
