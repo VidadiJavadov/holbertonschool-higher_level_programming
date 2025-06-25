5e83a57edf78cc967847050e9f63fecb79e16c90
VidadiJavadov <vidadi4000@gmail.com>
2025-06-25 19:46:45 +0400

    9-rectangle.py

---
+++ python-more_classes/9-rectangle.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+"""Rectangle Class"""
+
+
+class Rectangle:
+    """This a Rectangle class"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initializing"""
+        Rectangle.number_of_instances += 1
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter - width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter - width"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter - height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter - height"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Method for calculating area of rectangle"""
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """This a method for calculating perimetr of a rectangle"""
+
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Return rectangle as a string of '#' characters"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        s = str(self.print_symbol)
+        return "\n".join([s * self.__width for i in range(self.__height)])
+
+    def __repr__(self):
+        """Returning string"""
+
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Deconstructor"""
+        Rectangle.number_of_instances -= 1
+
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """This is a method for comparing two rectangle"""
+
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if (rect_1.area() >= rect_2.area()):
+            return rect_1
+        return rect_2
+    
+    @classmethod
+    def square(cls, size=0):
+        """This a classmethod that returns a square rectangle"""
+        return Rectangle(size, size)
