cada1e3695fe7e693ce01b4b5a19ca8bd067bbab
VidadiJavadov <vidadi4000@gmail.com>
2025-06-25 18:24:06 +0400

    Update 2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -42,12 +42,14 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
     
-     def area(self):
+    def area(self):
         """Method for calculating area of rectangle"""
-        return self.__width * self.__height
 
+        return self.__width * self.__height
+    
     def perimeter(self):
         """This a method for calculating perimetr of a rectangle"""
+        
         if self.__height == 0 or self.__width == 0:
-            return 0
+            return 0 
         return (self.__width + self.__height) * 2
