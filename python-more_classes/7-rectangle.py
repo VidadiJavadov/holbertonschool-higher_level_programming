80ce1ffad1e4d9cbfcd89ed37a48e750f60fee22
VidadiJavadov <vidadi4000@gmail.com>
2025-06-25 19:34:29 +0400

    Update 7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializing"""
-        Reactangle.number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
         self.width = width
         self.height = height
