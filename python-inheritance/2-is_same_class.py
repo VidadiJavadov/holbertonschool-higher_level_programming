d699fa55368dcafc68a7255a2aabd10ccd33605d
VidadiJavadov <vidadi4000@gmail.com>
2025-06-26 15:53:04 +0400

    2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This file contains a function"""
+
+
+def is_same_class(obj, a_class):
+    """This is a function that checks if an object is instance of a class"""
+
+    if isinstance(obj, a_class):
+        return True
+    return False
