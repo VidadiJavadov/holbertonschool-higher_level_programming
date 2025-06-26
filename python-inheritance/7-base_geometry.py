ac19a2c29b3a0bef782341ab33b3bce53c1997d9
VidadiJavadov <vidadi4000@gmail.com>
2025-06-26 18:31:11 +0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
     
     def area(self):
         """A method for area"""
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
     
     def integer_validator(self, name, value):
         """A method for validating"""
