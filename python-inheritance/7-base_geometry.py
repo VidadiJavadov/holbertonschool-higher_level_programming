ab937d8038497430742eb6a49746db28520c5958
VidadiJavadov <vidadi4000@gmail.com>
2025-06-26 17:00:17 +0400

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This file contains a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    
+    def area(self):
+        """A method for area"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """A method for validating"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
