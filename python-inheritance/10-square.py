45570ae0ab428f8c362c1693f5f5dbd3a749b6dd
VidadiJavadov <vidadi4000@gmail.com>
2025-06-26 17:45:24 +0400

    10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A file contains Square1 class"""
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """A class for square"""
+    def __init__(self, size):
+        """Initialization of Square"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return super().area()
