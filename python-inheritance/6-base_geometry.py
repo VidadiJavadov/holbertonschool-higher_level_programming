25239f76ef4530992d74272580b756c90f6292e9
VidadiJavadov <vidadi4000@gmail.com>
2025-06-26 16:43:56 +0400

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This file contains a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
