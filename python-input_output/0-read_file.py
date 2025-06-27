3de14f483757e89c95dafdf6a3d364d6794ea9e5
VidadiJavadov <vidadi4000@gmail.com>
2025-06-27 22:35:56 +0400

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this file contains a read_file function"""
+
+
+def read_file(filename=""):
+    """This is that function which prints content of file"""
+
+    with open(filename, mode="r", encoding="utf-8") as myfile:
+        print(myfile.read())
