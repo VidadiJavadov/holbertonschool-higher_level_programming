0f8a600b93fcf72b7fef57efe14bd8963f269f1d
VidadiJavadov <vidadi4000@gmail.com>
2025-06-27 22:48:59 +0400

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """This is that function which prints content of file"""
 
     with open(filename, encoding="utf-8") as myfile:
-        print(myfile.readlines(), end="")
+        print(myfile.read(), end="")
