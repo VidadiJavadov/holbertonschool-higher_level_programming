43825c422caf91e9092a0f223f80f7774698390f
VidadiJavadov <vidadi4000@gmail.com>
2025-06-28 07:20:24 +0400

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """This is a list to json"""
 import json
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
