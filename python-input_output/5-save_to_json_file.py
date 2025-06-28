0015e450e369434a122b5050ac9453df0c385a16
VidadiJavadov <vidadi4000@gmail.com>
2025-06-28 06:37:39 +0400

    Update 5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """func"""
     with open(filename, mode="w") as myfile:
-        json.dumbs(my_obj, myfile)
+        json.dumb(my_obj, myfile)
