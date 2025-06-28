3b300eb1dcae327e8b0a7ae1c77630061ca3369d
VidadiJavadov <vidadi4000@gmail.com>
2025-06-28 10:12:36 +0400

    :11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict representation"""
+        result = {}
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if(all(type(attr)) is str):
+                    for key in attrs:
+                        if hasattr(self, key):
+                            result[key] = getattr(self, key)
+        return self.__dict__
+    
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        with those from the json dictionary.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
