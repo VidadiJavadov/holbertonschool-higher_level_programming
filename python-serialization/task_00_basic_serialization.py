896c3482173bd6551f9ee58ee6773b746d785077
VidadiJavadov <vidadi4000@gmail.com>
2025-06-30 16:17:45 +0400

    task_00_basic_serialization.py

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""Serialization"""
+
+
+def serialize_and_save_to_file(data, filename):
+    """Ser_and_save"""
+    with open(filname, mode="w") as file:
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    """load_and_deser"""
+    with open(filename, mode="r") as file:
+        data = json.load(file)
