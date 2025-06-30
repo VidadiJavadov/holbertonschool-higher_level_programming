fac6d930683708101868cca5fb69631dabca6a14
VidadiJavadov <vidadi4000@gmail.com>
2025-06-30 17:08:40 +0400

    Update task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -11,18 +11,23 @@ class CustomObject:
     age: int
     is_student: bool
 
+    def __init__(self, name, age, is_student):
+        """init"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
     def display(self):
         """display"""
         print("Name: {} \n Age: {} \n Is Student: {}".format(self.name, self.age, self.is_student))
 
     def serialize(self, filename):
         """serializing"""
-        with open(filename, mode="w") as file:
+        with open(filename, mode="wb") as file:
             pickle.dump(self, file)
 
     @classmethod
     def deserialize(cls, filename):
         """deserializing"""
-        with open(filename, mode="r") as file:
+        with open(filename, mode="rb") as file:
             return (pickle.load(file))
-
