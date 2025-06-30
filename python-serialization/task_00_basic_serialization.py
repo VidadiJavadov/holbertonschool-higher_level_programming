e88adad4c9c9864f2392f5e58b57315894801ec4
VidadiJavadov <vidadi4000@gmail.com>
2025-06-30 16:22:40 +0400

    Update task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,10 +5,11 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     """Ser_and_save"""
-    with open(filname, mode="w") as file:
+    with open(filename, mode="w") as file:
         json.dump(data, file)
 
 def load_and_deserialize(filename):
     """load_and_deser"""
     with open(filename, mode="r") as file:
         data = json.load(file)
+        return data
