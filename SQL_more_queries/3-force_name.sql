5c79b3f1b47503b8a81baf837382b6bc7a84b097
VidadiJavadov <vidadi4000@gmail.com>
2025-07-03 13:30:45 +0400

    Update 3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -4,4 +4,5 @@ CREATE TABLE IF NOT EXISTS force_name (
     name VARCHAR(256) NOT NULL
 );
 
-GRANT ALL PRIVILEGES ON hbtn_0d_2.force_table TO 'user_0d_2'@'localhost'
+GRANT ALL PRIVILEGES ON hbtn_0d_2.force_table TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
