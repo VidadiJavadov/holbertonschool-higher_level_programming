b5adf5953acbbe82626cd64d756ac3678d5b2eff
VidadiJavadov <vidadi4000@gmail.com>
2025-07-03 07:42:58 +0400

    3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,9 +1,5 @@
 --Creating table 
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
-
-INSERT INTO force_name (id, name) VALUES
-(1, 'Holberton School'),
-(1, 'Python is cool'),
-(2, 'Holberton'),
-(3, 'School'),
-(4, 'C is fun');
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
