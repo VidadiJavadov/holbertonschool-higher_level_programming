eafa0b7dcc0a10bf6f4b867bfb35979541ad939a
VidadiJavadov <vidadi4000@gmail.com>
2025-07-03 13:44:49 +0400

    4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,4 @@
 -- ID cant be null
-CREATE TABLE IF NOT EXISTS id_not_null (id DEFAULT 1, name VARCHAR(256));
-GRANT ALL PRIVILEGES ON force_name TO 'user'@'localhost';
+CREATE TABLE IF NOT EXISTS id_not_null (
+id DEFAULT 1,
+name VARCHAR(256));
