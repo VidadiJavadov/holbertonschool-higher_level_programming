ebe9e2d866918073e4bc915bf8c7fa9885310600
VidadiJavadov <vidadi4000@gmail.com>
2025-07-03 07:52:22 +0400

    3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,5 @@
 --Creating table 
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256)
 );
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
