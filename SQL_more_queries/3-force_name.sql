eaa06560a098325cd3a8beeb8709253bcc42521b
VidadiJavadov <vidadi4000@gmail.com>
2025-07-03 13:37:51 +0400

    Update 3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -3,6 +3,3 @@ CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
-
-GRANT ALL PRIVILEGES ON hbtn_0d_2.force_table TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
