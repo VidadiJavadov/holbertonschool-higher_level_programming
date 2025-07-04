e0771bd65cf4eef8cd2fb8224df721157df3d8a4
VidadiJavadov <vidadi4000@gmail.com>
2025-07-04 23:44:36 +0400

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 id INT AUTO_INCREMENT PRIMARY KEY,
-state_id FOREIGN KEY(id) REFERENCES states(id),
-name NOT NULL
+state_id INT FOREIGN KEY(id) REFERENCES states(id),
+name VARCHAR(256) NOT NULL
 );
